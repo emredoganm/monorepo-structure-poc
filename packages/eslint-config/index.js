679e049833470a8b6f6b3dfabe4a63d35488380a
Emre Dogan <emredoganm@live.com>
2021-11-16 11:01:09 +0300

    Change eslint configuration file type

--- packages/eslint-config/index.js
+++ packages/eslint-config/index.js
@@ -1,3 +1,19 @@
-const config = require("./.eslintrc.json");
-
-module.exports = config;
+module.exports = {
+  env: {
+    browser: true,
+    es6: true,
+    node: true,
+  },
+  extends: ["standard", "plugin:react/recommended", "plugin:prettier/recommended"],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  settings: { react: { version: "detect" } },
+  plugins: ["react", "@typescript-eslint"],
+  rules: {},
+};
